6969630246caf164afba6ec1f470b2e03e02b3b3
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2021-09-13 18:49:12 +0300

    download btn icon
    added download icons for linux/windows download btns

--- static/js/shutdowntimer.js
+++ static/js/shutdowntimer.js
@@ -1,2 +1,10 @@
 const windows = document.querySelector(".fa-windows")
-const download = document.querySelector("fas fa-arrow-down")
+const linux = document.querySelector(".fa-linux")
+const windows_btn = document.querySelector(".btn-outline-primary")
+const linux_btn = document.querySelector(".btn-outline-warning")
+windows_btn.onclick = function(){
+    windows.className = "fas fa-arrow-down"
+}
+linux_btn.onclick = function(){
+    linux.className = "fas fa-arrow-down"
+}
