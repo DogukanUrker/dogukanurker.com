8fbddfd7b3abd676f0ce2961fcf3c6f14216927e
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2021-09-13 19:06:37 +0300

    Update shutdowntimer.js

--- static/js/shutdowntimer.js
+++ static/js/shutdowntimer.js
@@ -2,9 +2,11 @@ const windows = document.querySelector(".fa-windows")
 const linux = document.querySelector(".fa-linux")
 const windows_btn = document.querySelector(".btn-outline-primary")
 const linux_btn = document.querySelector(".btn-outline-warning")
+window.onload = function(){
+    document.querySelector(".btn-outline-warning").onclick=function(){
+        linux.className = "fas fa-arrow-down";
+    }
+}
 windows_btn.onclick = function(){
     windows.className = "fas fa-arrow-down"
-}
-linux_btn.onclick = function(){
-    linux.className = "fas fa-arrow-down"
 }
