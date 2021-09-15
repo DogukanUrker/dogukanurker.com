9befa636dd72cb595dcd2194242a2ba281903ed9
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2021-09-15 17:36:35 +0300

    ‎

--- app.py
+++ app.py
@@ -27,6 +27,10 @@ def page_not_found(e):
 def shutdowntimer_exe():
     file = "ShutdownTimer.exe"
     return send_file(file,as_attachment=True)
+@app.route('/shutdowntimer')
+def shutdowntimer_exe():
+    file = "ShutdownTimer"
+    return send_file(file,as_attachment=True)
 @app.route('/shutdowntimersetup.exe')
 def shutdowntimersetup_exe():
     file = "ShutdownTimer-Setup.exe"
