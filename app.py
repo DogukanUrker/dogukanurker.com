d8ac78f7d496d34de185c0100d04f3e40668c22e
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2022-01-09 18:48:42 +0300

    ㅤ

--- app.py
+++ app.py
@@ -1,42 +1,67 @@
-from flask import Flask,render_template,redirect,url_for,send_file
+from flask import Flask, render_template, redirect, url_for, send_file
 from flask_sslify import SSLify
+
 app = Flask(__name__)
 sslify = SSLify(app)
-@app.route('/')
+
+
+@app.route("/")
 def index():
     return redirect(url_for("socials"))
-@app.route('/main')
+
+
+@app.route("/main")
 def main():
     return render_template("mainpage.html")
-@app.route('/socials')
+
+
+@app.route("/socials")
 def socials():
     return render_template("socials.html")
-@app.route('/shutdowntimer')
+
+
+@app.route("/shutdowntimer")
 def shutdowntimer():
     return render_template("shutdowntimer.html")
-@app.route('/apps')
+
+
+@app.route("/apps")
 def projects():
     return render_template("apps.html")
-@app.route('/all')
+
+
+@app.route("/all")
 def all():
     return render_template("all.html")
+
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
-@app.route('/fiverr')
+    return render_template("404.html"), 404
+
+
+@app.route("/fiverr")
 def fiverr():
     return render_template("fiverr.html")
-@app.route('/shutdowntimer.exe')
+
+
+@app.route("/shutdowntimer.exe")
 def shutdowntimer_exe():
     file = "ShutdownTimer/ShutdownTimer.exe"
-    return send_file(file,as_attachment=True)
-@app.route('/shutdowntimer-linux')
+    return send_file(file, as_attachment=True)
+
+
+@app.route("/shutdowntimer-linux")
 def shutdowntimer_linux():
     file = "ShutdownTimer/ShutdownTimer"
-    return send_file(file,as_attachment=True)
-@app.route('/shutdowntimersetup.exe')
+    return send_file(file, as_attachment=True)
+
+
+@app.route("/shutdowntimersetup.exe")
 def shutdowntimersetup_exe():
     file = "ShutdownTimer-Setup.exe"
-    return send_file(file,as_attachment=True)
-if __name__ == '__main__':
+    return send_file(file, as_attachment=True)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- static/js/shutdowntimer.js
+++ static/js/shutdowntimer.js
@@ -1,10 +1,10 @@
-const windows = document.querySelector(".fa-windows")
-const linux = document.querySelector(".fa-linux")
-const windows_btn = document.querySelector(".btn-outline-primary")
-const linux_btn = document.querySelector(".btn-outline-warning")
-linux_btn.onclick = function(){
-    linux.className = "fas fa-arrow-down";
-}
-windows_btn.onclick = function(){
-    windows.className = "fas fa-arrow-down";
-}
+const windows = document.querySelector(".fa-windows");
+const linux = document.querySelector(".fa-linux");
+const windows_btn = document.querySelector(".btn-outline-primary");
+const linux_btn = document.querySelector(".btn-outline-warning");
+linux_btn.onclick = function () {
+  linux.className = "fas fa-arrow-down";
+};
+windows_btn.onclick = function () {
+  windows.className = "fas fa-arrow-down";
+};
