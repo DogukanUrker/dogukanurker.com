f8b6b4576be3dc4e0fe3887922598a8e2e7dce9e
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2022-02-07 23:48:37 +0300

    ㅤ

--- static/js/shutdowntimer.js
+++ static/js/shutdowntimer.js
@@ -1,11 +1,6 @@
 console.log("shutdowntimer.js loaded");
 const windows = document.querySelector(".fa-windows");
-const linux = document.querySelector(".fa-linux");
 const windows_btn = document.querySelector(".btn-outline-primary");
-const linux_btn = document.querySelector(".btn-outline-warning");
-linux_btn.onclick = function () {
-  linux.className = "fas fa-arrow-down";
-};
 windows_btn.onclick = function () {
   windows.className = "fas fa-arrow-down";
 };
