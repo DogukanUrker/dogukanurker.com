696b508e5f28a66d0306682fed1b79084d9e42fd
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2023-02-13 16:15:51 +0300

    remake of /shutdowntimer

--- static/js/shutdowntimer.js
+++
@@ -1,5 +0,0 @@
-const iconWin = document.querySelector(".fa-windows");
-const btnWin = document.querySelector(".btnWin");
-btnWin.onclick = function () {
-  iconWin.className = "fas fa-arrow-down";
-};
