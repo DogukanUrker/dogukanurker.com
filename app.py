7cc9bf24c31f9ca9efa4924a92ef8c5edcee9e87
Doğukan Ürker <dogukanurker@icloud.com>
2024-06-25 01:34:51 +0300

    feat: create new routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, send_file
 from flask_sslify import SSLify
 
 app = Flask(__name__)
@@ -6,9 +6,28 @@ sslify = SSLify(app)
 
 
 @app.route("/")
-def hello():
+def home():
     return render_template("index.html")
 
+@app.errorhandler(404)
+def notFound():
+    return redirect(url_for('home'))
+
+@app.route("/cv")
+def cv():
+    return send_file("static/cv.pdf")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/socials")
+def socials():
+    return render_template("socials.html")
+
+@app.route("/donate")
+def donate():
+    return redirect("https://www.buymeacoffee.com/dogukanurker")
 
 if __name__ == "__main__":
     app.run(debug=True)
