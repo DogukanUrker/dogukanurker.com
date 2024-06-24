c5b1290cfc846d17a62f37702fb4df51f5f5c99f
Doğukan Ürker <dogukanurker@icloud.com>
2024-06-25 02:55:50 +0300

    feat: query to GitHub API

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, send_file
 from flask_sslify import SSLify
+from requests import get
 
 app = Flask(__name__)
 sslify = SSLify(app)
@@ -9,25 +10,35 @@ sslify = SSLify(app)
 def home():
     return render_template("index.html")
 
+
 @app.errorhandler(404)
-def notFound():
-    return redirect(url_for('home'))
+def notFound(e):
+    return redirect(url_for("home"))
+
 
 @app.route("/cv")
 def cv():
     return send_file("static/cv.pdf")
 
+
 @app.route("/projects")
 def projects():
-    return render_template("projects.html")
+    response = get("https://api.github.com/users/dogukanurker")
+    data = response.json()
+    followerCount = data["followers"]
+    repoCount = data["public_repos"]
+    return render_template("projects.html", followers=followerCount, repos=repoCount)
+
 
 @app.route("/socials")
 def socials():
     return render_template("socials.html")
 
+
 @app.route("/donate")
 def donate():
     return redirect("https://www.buymeacoffee.com/dogukanurker")
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="192.168.1.70")
