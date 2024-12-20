fb6ee92b08eed1d901b959bdd85349e0a0e57935
Doğukan Ürker <dogukanurker@icloud.com>
2024-12-20 15:28:28 +0300

    basic index page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,8 +1,34 @@
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
 
 export default function Home() {
-  return (
-      <div className="h-screen flex items-center justify-center select-none text-4xl font-bold">
-          <h1>Dogukan Ürker</h1>
-      </div>
-  );
+    return (
+        <div className="h-screen flex flex-col items-center justify-center select-none text-4xl font-bold">
+            <div className={"mb-4"}><Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href="https://x.com/dogukanurker" target={"_blank"}> X</BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator/>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href="https://github.com/dogukanurker" target={"_blank"}>GitHub</BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator/>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href="https://youtube.com/@dogukanurker"
+                                        target={"_blank"}>Youtube</BreadcrumbLink>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            </div>
+            <h1>Doğukan Ürker</h1>
+            <p className={"text-base mt-4"}>Building apps</p>
+        </div>
+    );
 }
