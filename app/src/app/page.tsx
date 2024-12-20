7f55dc163be4112e9a7c30697ee1539cff70d246
Doğukan Ürker <dogukanurker@icloud.com>
2024-12-20 14:55:16 +0300

    add next and shadcn

---
+++ app/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+      <div className="h-screen flex items-center justify-center select-none text-4xl font-bold">
+          <h1>Dogukan Ürker</h1>
+      </div>
+  );
+}
