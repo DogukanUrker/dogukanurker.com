ac77688fbce5fcc983be64225937c673678d4c25
Doğukan Ürker <dogukanurker@icloud.com>
2024-12-26 19:32:38 +0300

    feat: letters

---
+++ app/src/app/letter/[letter]/page.tsx
@@ -0,0 +1,28 @@
+import {notFound} from 'next/navigation';
+
+interface PageProps {
+    params: Promise<{
+        letter: string;
+    }>;
+}
+
+export default async function Page({params}: PageProps) {
+    const {letter} = await params;
+    const letterPath = `/letters/${letter}.pdf`;
+
+    if (!letter) {
+        notFound();
+    }
+    return (
+        <div className="container mx-auto py-10">
+            <div className="relative rounded-lg overflow-hidden h-[calc(100vh-5rem)]">
+                <embed
+                    src={letterPath}
+                    type="application/pdf"
+                    className="w-full h-full object-contain"
+                />
+            </div>
+        </div>
+    )
+
+}
