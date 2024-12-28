fd45c1dedeb24f1eb8047cb7f83f35d606a0eddb
Doğukan Ürker <dogukanurker@icloud.com>
2024-12-28 14:35:06 +0300

    navbar fix on safari

--- app/src/components/navbar.tsx
+++ app/src/components/navbar.tsx
@@ -7,7 +7,7 @@ export function Navbar() {
         <motion.header
             initial={{y: -100}}
             animate={{y: 0}}
-            className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+            className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 bg-background"
         >
             <div className="container flex h-14 items-center justify-center">
                 <nav className="flex items-center space-x-8 text-sm font-medium">
