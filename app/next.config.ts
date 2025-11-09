adc0ea889151db42a1b35521a7d7ffc9d334d99c
Doğukan Ürker <dogukanurker@icloud.com>
2025-11-09 20:08:08 +0300

    Update image host and improve formatting in blog post
    Changed allowed image hostname in next.config.ts from Unsplash to Contentful. Updated the blog post to use a new banner image from Contentful, improved formatting for Callout and TimelineItem components, added an example ImageWithCaption, and standardized string quotes in code samples.

--- app/next.config.ts
+++ app/next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.ctfassets.net",
       },
     ],
   },
