fb43bb8f9cd7560577a022565fdf1117ca442629
Doğukan Ürker <dogukanurker@icloud.com>
2025-12-20 02:46:13 +0300

    Add pbs.twimg.com to allowed image domains
    Updated next.config.ts to include pbs.twimg.com in the list of allowed image domains for Next.js image optimization.

--- app/next.config.ts
+++ app/next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.ctfassets.net",
       },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+      },
     ],
   },
 };
